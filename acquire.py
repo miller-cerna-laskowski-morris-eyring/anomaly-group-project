a806bfcb3d57c73b9acafb2cc1097eb849950d84
Ryan Miller <ryan.d.miller05@gmail.com>
2022-07-07 17:21:48 -0500

    updated with cohort dates and program type

--- acquire.py
+++ acquire.py
@@ -23,7 +23,10 @@ def get_curriculum_data():
                logs.path,
                logs.user_id,
                logs.ip,
-               cohorts.name
+               cohorts.name,
+               cohorts.start_date,
+               cohorts.end_date,
+               cohorts.program_id
             FROM
                 logs
             LEFT JOIN
